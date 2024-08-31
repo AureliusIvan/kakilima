521f29918edbee8681c67eb85c1eeab5f7e36523
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-08-31 17:10:43 +0700

    feat/add-home-banner

--- src/app/(core)/category/layout.tsx
+++ src/app/(core)/category/layout.tsx
@@ -1,5 +1,6 @@
 import {CategoryMenu} from "@/components/layout/navbar/navbar";
 import React from "react";
+import Image from "next/image";
 
 export default async function DetailLayout(
     {
@@ -10,6 +11,14 @@ export default async function DetailLayout(
 ) {
   return (
       <>
+        <Image
+            className={`w-full h-[400px] object-cover object-center`}
+            src={'/images/home-banner.webp'}
+            width={1920}
+            height={1080}
+            alt={'KakiLima'}
+
+        />
         <CategoryMenu/>
         <section className={`p-6`}>
           {children}
